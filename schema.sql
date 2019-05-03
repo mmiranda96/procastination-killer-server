6897742c8f6743b0ccf582472824f025e2462db6
Miguel Miranda <mikemp96@gmail.com>
2019-05-02 21:49:37 -0500

    Update schema

--- schema.sql
+++ schema.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 10.4 (Debian 10.4-1.pgdg90+1)
 -- Dumped by pg_dump version 11.2
 
--- Started on 2019-05-02 16:46:15 UTC
+-- Started on 2019-05-03 02:49:10 UTC
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -57,7 +57,7 @@ CREATE SEQUENCE public.subtasks_id_seq
 
 
 --
--- TOC entry 2888 (class 0 OID 0)
+-- TOC entry 2889 (class 0 OID 0)
 -- Dependencies: 198
 -- Name: subtasks_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
@@ -95,7 +95,7 @@ CREATE SEQUENCE public.tasks_id_seq
 
 
 --
--- TOC entry 2889 (class 0 OID 0)
+-- TOC entry 2890 (class 0 OID 0)
 -- Dependencies: 196
 -- Name: tasks_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
@@ -112,7 +112,8 @@ CREATE TABLE public.users (
     id integer NOT NULL,
     email character varying(64) NOT NULL,
     password character varying(128) NOT NULL,
-    name character varying(64)
+    name character varying(64),
+    firebase_token character varying(256)
 );
 
 
@@ -131,7 +132,7 @@ CREATE SEQUENCE public.users_id_seq
 
 
 --
--- TOC entry 2890 (class 0 OID 0)
+-- TOC entry 2891 (class 0 OID 0)
 -- Dependencies: 200
 -- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
@@ -151,7 +152,7 @@ CREATE TABLE public.users_tasks (
 
 
 --
--- TOC entry 2747 (class 2604 OID 24851)
+-- TOC entry 2748 (class 2604 OID 24851)
 -- Name: subtasks id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -159,7 +160,7 @@ ALTER TABLE ONLY public.subtasks ALTER COLUMN id SET DEFAULT nextval('public.sub
 
 
 --
--- TOC entry 2746 (class 2604 OID 24840)
+-- TOC entry 2747 (class 2604 OID 24840)
 -- Name: tasks id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -167,7 +168,7 @@ ALTER TABLE ONLY public.tasks ALTER COLUMN id SET DEFAULT nextval('public.tasks_
 
 
 --
--- TOC entry 2748 (class 2604 OID 24864)
+-- TOC entry 2749 (class 2604 OID 24864)
 -- Name: users id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -175,7 +176,7 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 
 
 --
--- TOC entry 2758 (class 2606 OID 33933)
+-- TOC entry 2759 (class 2606 OID 33933)
 -- Name: reset_password_tokens reset_password_tokens_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -184,7 +185,7 @@ ALTER TABLE ONLY public.reset_password_tokens
 
 
 --
--- TOC entry 2752 (class 2606 OID 24853)
+-- TOC entry 2753 (class 2606 OID 24853)
 -- Name: subtasks subtasks_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -193,7 +194,7 @@ ALTER TABLE ONLY public.subtasks
 
 
 --
--- TOC entry 2750 (class 2606 OID 24845)
+-- TOC entry 2751 (class 2606 OID 24845)
 -- Name: tasks tasks_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -202,7 +203,7 @@ ALTER TABLE ONLY public.tasks
 
 
 --
--- TOC entry 2754 (class 2606 OID 24866)
+-- TOC entry 2755 (class 2606 OID 24866)
 -- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -211,7 +212,7 @@ ALTER TABLE ONLY public.users
 
 
 --
--- TOC entry 2756 (class 2606 OID 24871)
+-- TOC entry 2757 (class 2606 OID 24871)
 -- Name: users_tasks users_tasks_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -220,7 +221,7 @@ ALTER TABLE ONLY public.users_tasks
 
 
 --
--- TOC entry 2759 (class 2606 OID 24854)
+-- TOC entry 2760 (class 2606 OID 24854)
 -- Name: subtasks FK_task_id; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -229,7 +230,7 @@ ALTER TABLE ONLY public.subtasks
 
 
 --
--- TOC entry 2761 (class 2606 OID 24877)
+-- TOC entry 2762 (class 2606 OID 24877)
 -- Name: users_tasks FK_task_id; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -238,7 +239,7 @@ ALTER TABLE ONLY public.users_tasks
 
 
 --
--- TOC entry 2760 (class 2606 OID 24872)
+-- TOC entry 2761 (class 2606 OID 24872)
 -- Name: users_tasks FK_user_id; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -246,7 +247,7 @@ ALTER TABLE ONLY public.users_tasks
     ADD CONSTRAINT "FK_user_id" FOREIGN KEY (user_id) REFERENCES public.users(id) ON UPDATE CASCADE ON DELETE CASCADE;
 
 
--- Completed on 2019-05-02 16:46:15 UTC
+-- Completed on 2019-05-03 02:49:10 UTC
 
 --
 -- PostgreSQL database dump complete
